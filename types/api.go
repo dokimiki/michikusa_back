824212f4cd2bd536456b7fb69bee2a985147ae1a
eno1220 <eno1220dev@gmail.com>
2024-08-03 14:52:04 +0900

    feat: add types for request and response

---
+++ types/api.go
@@ -0,0 +1,39 @@
+package types
+
+type Facility struct {
+	Name string `json:"name"`
+	Distance float64 `json:"distance"` // バックエンド側で計算した距離、単位はメートル
+	Genre string `json:"genre"`
+	Latitude float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type Station struct {
+	Name string `json:"name"`
+	Latitude float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+// todo rename
+type InitialRequestData struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Price	 int `json:"price,omitempty"`
+}
+
+type InitialResponseData struct {
+	NearestStation Station `json:"nearest_station"`
+	DestinationStation Station `json:"destination_station"`
+	RailwayName string `json:"railway_name"`
+	RailwayColor string `json:"railway_color"`
+	NumerOfStations int `json:"number_of_stations"`
+	Facilities []Facility `json:"facilities"`
+}
+
+type LocationsRequestData struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type LocationsResponseData struct {
+	Facilities []Facility `json:"facilities"`
+}
