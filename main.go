b2a767eaa2bd9371ae202f3f7f185970e65da4c3
eno1220 <eno1220dev@gmail.com>
2024-08-03 15:07:39 +0900

    feat: add dummy data to endpoints

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"michikusa_back/types"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -17,16 +18,90 @@ func main() {
 
 	// 「現在地」の情報を受け取って、最寄駅や行き先駅、施設の情報を返すAPI
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{
-			"message": "Hello, World!",
-		})
+		var req types.InitialRequestData
+		if err := c.Bind(&req); err != nil {
+			// todo: fix エラーハンドリング
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"message": "Bad Request",
+			})
+		}
+
+		res := types.InitialResponseData{
+			NearestStation: types.Station{
+				Name: "新宿",
+				Latitude: 35.69237,
+				Longitude: 139.70121,
+			},
+			DestinationStation: types.Station{
+				Name: "東京",
+				Latitude: 35.6818,
+				Longitude: 139.7647,
+			},
+			RailwayName: "丸ノ内線",
+			RailwayColor: "#F62E36",
+			Facilities: []types.Facility{
+				{
+					Name: "東京国際フォーラム",
+					Distance: 500,
+					Genre: "コンベンションセンター",
+					Latitude: 35.6784,
+					Longitude: 139.7636,
+				},
+				{
+					Name: "皇居外苑",
+					Distance: 1000,
+					Genre: "公園",
+					Latitude: 35.6825,
+					Longitude: 139.7521,
+				},
+				{
+					Name: "東京ステーションギャラリー",
+					Distance: 0,
+					Genre: "美術館",
+					Latitude: 35.6812,
+					Longitude: 139.7671,
+				},
+			},
+		}
+		return c.JSON(http.StatusOK, res)
 	})
 	
 	// 「行き先駅」の情報を受け取って、付近の施設の情報を返すAPI
 	e.GET("/locations-list", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{
-			"message": "Locations List",
-		})
+		var req types.LocationsRequestData
+		if err := c.Bind(&req); err != nil {
+			// todo: fix エラーハンドリング
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"message": "Bad Request",
+			})
+		}
+		
+		res := types.LocationsResponseData{
+			Facilities: []types.Facility{
+				{
+					Name: "東京国際フォーラム",
+					Distance: 500,
+					Genre: "コンベンションセンター",
+					Latitude: 35.6784,
+					Longitude: 139.7636,
+				},
+				{
+					Name: "皇居外苑",
+					Distance: 1000,
+					Genre: "公園",
+					Latitude: 35.6825,
+					Longitude: 139.7521,
+				},
+				{
+					Name: "東京ステーションギャラリー",
+					Distance: 0,
+					Genre: "美術館",
+					Latitude: 35.6812,
+					Longitude: 139.7671,
+				},
+			},
+		}
+		return c.JSON(http.StatusOK, res)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
