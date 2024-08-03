b08291a74ca6e2e113f92ad1c88c97f4d3809fc8
dokimiki <57443812+dokimiki@users.noreply.github.com>
2024-08-03 17:51:10 +0900

    getFacilityかんせい！

--- getFacility.go
+++ getFacility.go
@@ -1,40 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"michikusa_back/types"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
-	"strings"
-
-	"github.com/labstack/echo/v4"
 )
 
-var API_KEY string
-
-func init() {
-	API_KEY = os.Getenv("YAHOO_API_KEY")
-	API_KEY = strings.ReplaceAll(API_KEY, "\n", "")
-	API_KEY = strings.ReplaceAll(API_KEY, "\r", "")
-}
-
-func GetFacility(c echo.Context) error {
-	var i types.LocationsRequestData
-	if err := c.Bind(&i); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"message": "Bad Request",
-		})
-	}
-
-	i.Latitude = 35.6907446
-	i.Longitude = 139.6881503
-
+func GetFacility(i types.LocationsRequestData, yahooApiKey string) (types.YDF, error) {
 	u, _ := url.Parse("https://map.yahooapis.jp/search/local/V1/localSearch")
 	q := u.Query()
-	q.Set("appid", API_KEY)
+	q.Set("appid", yahooApiKey)
 	q.Set("lat", strconv.FormatFloat(i.Latitude, 'f', -1, 64))
 	q.Set("lon", strconv.FormatFloat(i.Longitude, 'f', -1, 64))
 	q.Set("output", "json")
@@ -44,22 +23,22 @@ func GetFacility(c echo.Context) error {
 	req, _ := http.NewRequest("GET", u.String(), nil)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{
-			"message": "failed to send request",
-		})
+		return types.YDF{}, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return c.JSON(http.StatusInternalServerError, map[string]string{
-			"message": "api response is not ok(" + strconv.Itoa(resp.StatusCode) + ")",
-		})
+		return types.YDF{}, fmt.Errorf("api response is not ok(%d)", resp.StatusCode)
 	}
 	defer resp.Body.Close()
 
-	buf := new(strings.Builder)
-	io.Copy(buf, resp.Body)
-	fmt.Println(buf.String())
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return types.YDF{}, err
+	}
 
-	return c.JSON(http.StatusOK, map[string]string{
-		"message": "Hello, World!",
-	})
+	var facilities types.YDF
+	if err := json.Unmarshal(body, &facilities); err != nil {
+		return types.YDF{}, err
+	}
+
+	return facilities, nil
 }
